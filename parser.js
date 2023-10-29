1f4c66939cd2d900d12a9f7c2ec24bfc517e5a32
fromant <fromant65@gmail.com>
2023-10-29 11:11:54 -0300

    Cleaned parser.js code

--- parser.js
+++ parser.js
@@ -1,79 +1,63 @@
 const fs = require("fs");
 
 /**
+ * Parses a line and updates the card object accordingly.
+ * @param {string} line - The line to parse.
+ * @param {Object} card - The card object to update.
+ * @returns {Object} The updated card object.
+ */
+function parseLine(line, card) {
+  if (line.startsWith("Sentence: ")) {
+    card.sentence = line.substring(10);
+  } else if (line.startsWith("Translation: ")) {
+    card.translation = line.substring(13);
+  } else if (line.startsWith("Reading: ")) {
+    card.reading = line.substring(9);
+  } else if (line.startsWith("Grammar: ")) {
+    card.grammar = line.substring(9);
+    let grammarEntries = card.grammar.split(", ");
+    let formattedSentence = "";
+    let formattedGrammar = "";
+    for (let entry of grammarEntries) {
+      let [word, type] = entry.split(" (");
+      type = type.replace(")", "");
+      formattedSentence += `<span class="${type}">${word}</span> `;
+      formattedGrammar += `${word} (<span class="${type}">${type}</span>), `;
+    }
+    card.formattedSentence = formattedSentence.trim();
+    card.grammar = formattedGrammar.slice(0, -2);
+  }
+  return card;
+}
+
+/**
  * Parses a file and extracts information to create an array of cards.
  * @param {string} filename - The name of the file to parse.
  * @returns {Array} An array of cards containing the extracted information.
+ * @throws {Error} If the file does not exist or cannot be read.
  */
 function parseFile(filename) {
-  // Read the file synchronously
-  const data = fs.readFileSync(filename, "utf8");
-
-  // Split the data into lines
-  const lines = data.split("\n");
-
-  // Initialize an empty array to store the cards
-  const cards = [];
-
-  // Initialize an empty object to store the current card
-  let card = {};
-
-  // Iterate over each line in the file
-  for (let line of lines) {
-    if (line.startsWith("Sentence: ")) {
-      // If the line starts with "Sentence: ", create a new card object
-      // and set the sentence field
-      if (card.sentence) {
-        // If there is already a sentence in the current card object,
-        // push the card to the array and create a new card object
-        cards.push(card);
-        card = {};
-      }
-      card.sentence = line.substring(10);
-    } else if (line.startsWith("Translation: ")) {
-      // If the line starts with "Translation: ", set the translation field
-      card.translation = line.substring(13);
-    } else if (line.startsWith("Reading: ")) {
-      // If the line starts with "Reading: ", set the reading field
-      card.reading = line.substring(9);
-    } else if (line.startsWith("Grammar: ")) {
-      // If the line starts with "Grammar: ", set the grammar field
-      card.grammar = line.substring(9);
-
-      // Split the grammar entries by comma and space
-      let grammarEntries = card.grammar.split(", ");
-
-      // Initialize variables to store the formatted sentence and grammar
-      let formattedSentence = "";
-      let formattedGrammar = "";
-
-      // Iterate over each grammar entry
-      for (let entry of grammarEntries) {
-        // Split the entry into word and type
-        let [word, type] = entry.split(" (");
-
-        // Remove the closing parenthesis from the type
-        type = type.replace(")", "");
-
-        // Add the word with a span of the corresponding type to the formatted sentence
-        formattedSentence += `<span class="${type}">${word}</span> `;
-
-        // Add the word, type, and corresponding span to the formatted grammar
-        formattedGrammar += `${word} (<span class="${type}">${type}</span>), `;
-      }
-
-      // Trim the formatted sentence and remove the trailing comma from the formatted grammar
-      card.formattedSentence = formattedSentence.trim();
-      card.grammar = formattedGrammar.slice(0, -2);
-    }
+  if (!fs.existsSync(filename)) {
+    throw new Error(`The file ${filename} does not exist.`);
   }
 
-  // Push the last card to the array
+  const data = fs.readFileSync(filename, "utf8");
+  const lines = data.split("\n");
+  const cards = [];
+  let card = {};
+
+  for (let line of lines) {
+    if (line.startsWith("Sentence: ") && card.sentence) {
+      cards.push(card);
+      card = {};
+    }
+    card = parseLine(line, card);
+  }
+
   if (card.sentence) {
     cards.push(card);
   }
 
-  // Return the array of cards
   return cards;
 }
 
