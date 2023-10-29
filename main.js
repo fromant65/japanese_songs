2a2b9d3925befd5db20a4e021ad33e95cf683f80
fromant <fromant65@gmail.com>
2023-10-29 11:00:50 -0300

    cleaned main.js code

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const { parseFile } = require("./parser");
 const { convertToCsv } = require("./convert_to_csv");
 const fs = require("fs");
 
-const parsedText = parseFile("input.txt");
-const csvText = convertToCsv(parsedText);
-fs.writeFileSync("output.csv", csvText);
+try {
+  // Parse the input file
+  const parsedText = parseFile("input.txt");
+
+  // Convert the parsed text to CSV
+  const csvText = convertToCsv(parsedText);
+
+  // Write the CSV text to the output file
+  fs.writeFileSync("output.csv", csvText);
+
+  console.log("Conversion completed successfully!");
+} catch (error) {
+  console.error("An error occurred during the conversion:", error);
+}
