8def3c318ac74f47cc85600217281313cfe543a9
fromant <fromant65@gmail.com>
2023-11-14 15:53:27 -0300

    Made error handling better

--- parser.js
+++ parser.js
@@ -7,26 +7,31 @@ const fs = require("fs");
  * @returns {Object} The updated card object.
  */
 function parseLine(line, card) {
-  if (line.startsWith("Sentence: ")) {
-    card.sentence = line.substring(10);
-  } else if (line.startsWith("Translation: ")) {
-    card.translation = line.substring(13);
-  } else if (line.startsWith("Reading: ")) {
-    card.reading = line.substring(9);
-  } else if (line.startsWith("Grammar: ")) {
-    card.grammar = line.substring(9);
-    let grammarEntries = card.grammar.split(", ");
-    let formattedSentence = "";
-    let formattedGrammar = "";
-    for (let entry of grammarEntries) {
-      let [word, type] = entry.split(" (");
-      type = type.replace(")", "");
-      formattedSentence += `<span class="${type}">${word}</span> `;
-      formattedGrammar += `${word} (<span class="${type}">${type}</span>), `;
+  try {
+    if (line.startsWith("Sentence: ")) {
+      card.sentence = line.substring(10);
+    } else if (line.startsWith("Translation: ")) {
+      card.translation = line.substring(13);
+    } else if (line.startsWith("Reading: ")) {
+      card.reading = line.substring(9);
+    } else if (line.startsWith("Grammar: ")) {
+      card.grammar = line.substring(9);
+      let grammarEntries = card.grammar.split(", ");
+      let formattedSentence = "";
+      let formattedGrammar = "";
+      for (let entry of grammarEntries) {
+        let [word, type] = entry.split(" (");
+        type = type.replace(")", "");
+        formattedSentence += `<span class="${type}">${word}</span> `;
+        formattedGrammar += `${word} (<span class="${type}">${type}</span>), `;
+      }
+      card.formattedSentence = formattedSentence.trim();
+      card.grammar = formattedGrammar.slice(0, -2);
     }
-    card.formattedSentence = formattedSentence.trim();
-    card.grammar = formattedGrammar.slice(0, -2);
+  } catch (e) {
+    throw new Error(`There was an error in line \n${line}\n${e.message}`);
   }
+
   return card;
 }
 
